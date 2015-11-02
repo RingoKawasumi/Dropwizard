e6a79b171415580027ab38ae64e718fb8d619c7e
zhujie <zhujie@51zhangdan.com.cn>
2015-11-02 17:30:59 +0800

    初始化Dropwizard工程

---
+++ src/main/com/zhujie/study/GettingStarted/HelloWorldConfiguration.java
@@ -0,0 +1,9 @@
+package com.zhujie.study.GettingStarted;
+
+import io.dropwizard.Configuration;
+
+/**
+ * Created by zhujie on 15/11/2.
+ */
+public class HelloWorldConfiguration extends Configuration {
+}
